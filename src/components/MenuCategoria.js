ffb60d993de09fff1eb3f863f265801082e9e1af
Marleidis <90942857+Marleidis@users.noreply.github.com>
2021-12-21 00:35:51 -0500

    Se simplifica código

--- src/components/MenuCategoria.js
+++ src/components/MenuCategoria.js
@@ -1,5 +1,18 @@
+import ItemSubasta from './ItemSubasta';
 
-function MenuCategoria () {
+
+function MenuCategoria (props) {
+  
+  const subastas = props.subastas;
+  
+  const listaSubastas = subastas.map( s => 
+    <ItemSubasta 
+        key={s.id}
+        eliminarSubasta={ props.eliminarSubasta } 
+        {...s}
+        />
+  );
+
   return (
     <div className="row shadow p-3 mb-5 bg-body rounded property mx-4">
       <div className="col-sm-3 property my-4 ">
@@ -29,101 +42,7 @@ function MenuCategoria () {
                     </thead>
                     
                     <tbody>
-                        <tr >
-                            <th scope="row" >
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="radioNoLabel" 
-                                    id="radioNoLabel1" value="" aria-label="..."/>
-                                </div>
-                            </th>
-                            <td>A21-001</td>
-                            <td>Camioneta Honda</td>
-                            <td>$34.500.000</td>
-                            <td>
-                                <button type="button" className="btn btn-success btn-sm property px-3 mr-3">Editar</button>
-                                <button type="button" className="btn btn-danger btn-sm property me-2">Eliminar</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <th scope="row">
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="radioNoLabel" 
-                                    id="radioNoLabel1" value="" aria-label="..."/>
-                                </div>
-                            </th>
-                            <td>B21-001</td>
-                            <td>Bus</td>
-                            <td>$115.000.000</td>
-                            <td>
-                                <button type="button" className="btn btn-success btn-sm property px-3 mr-3">Editar</button>
-                                <button type="button" className="btn btn-danger btn-sm property me-2">Eliminar</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <th scope="row">
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="radioNoLabel" 
-                                    id="radioNoLabel1" value="" aria-label="..."/>
-                                </div>
-                            </th>
-                            <td>C21-001</td>
-                            <td>Camión JAC</td>
-                            <td>$63.200.000</td>
-                            <td>
-                                <button type="button" className="btn btn-success btn-sm property px-3 mr-3">Editar</button>
-                                <button type="button" className="btn btn-danger btn-sm property me-2">Eliminar</button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                        <th scope="row">
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="radioNoLabel" 
-                                id="radioNoLabel1" value="" aria-label="..."/>
-                            </div>
-                        </th>
-                        <td>C21-001</td>
-                        <td>Camión Chevrolet</td>
-                        <td>$79.000.000</td>
-                        <td>
-                            <button type="button" className="btn btn-success btn-sm property px-3 mr-3">Editar</button>
-                            <button type="button" className="btn btn-danger btn-sm property me-2">Eliminar</button>
-                        </td>
-                        </tr>
-
-                        <tr>
-                        <th scope="row">
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="radioNoLabel" 
-                                id="radioNoLabel1" value="" aria-label="..."/>
-                            </div>
-                        </th>
-                        <td>M21-001</td>
-                        <td>Moto Bajaj</td>
-                        <td>$4.100.000</td>
-                        <td>
-                            <button type="button" className="btn btn-success btn-sm property px-3 mr-3">Editar</button>
-                            <button type="button" className="btn btn-danger btn-sm property me-2">Eliminar</button>
-                        </td>
-                        </tr>
-
-                        <tr>
-                        <th scope="row">
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="radioNoLabel" 
-                                id="radioNoLabel1" value="" aria-label="..."/>
-                            </div>
-                        </th>
-                        <td>A21-001</td>
-                        <td>Spark GT</td>
-                        <td>$25.200.000</td>
-                        <td>
-                            <button type="button" className="btn btn-success btn-sm property px-3 mr-3">Editar</button>
-                            <button type="button" className="btn btn-danger btn-sm property me-2">Eliminar</button>
-                        </td>
-                        </tr>
+                      { listaSubastas }
                     </tbody>
                 </table>
             </div>
