48f824ccc7e8c253ba07e319164529451f5292a5
Marleidis <90942857+Marleidis@users.noreply.github.com>
2021-12-21 00:32:02 -0500

    Se actualiza nombre de los campos

--- src/components/DatosSubasta.js
+++ src/components/DatosSubasta.js
@@ -4,30 +4,30 @@ function DatosSubasta () {
     return (
         <div>
             <Form >
-                <div className="border border-primary property m-2 p-3" >
+                <div className="border shadow property m-3 p-3" >
                     <div className="row mb-3" bordercolor="blue">
-                        <label name="IdSubasta" className="col-sm-2 col-form-label" >Id Subasta:</label>
-                        <input type="text" className="col-sm-3" id="IdSubasta" placeholder="Id Subasta"/>
+                        <label name="id" className="col-sm-2 col-form-label" >Id Subasta:</label>
+                        <input type="text" className="col-sm-3" name="id" placeholder="Id Subasta"/>
                         <label name="precio" className="col-sm-2 col-form-label">Precio base:</label>
-                        <input type="number" className="col-sm-4" id="precio" placeholder="Precio base"/>
+                        <input type="number" className="col-sm-4" name="valorPuja" placeholder="Precio base"/>
                     </div>
                     <div className="row mb-3" bordercolor="blue">
                         <label name="IdSubasta" className="col-sm-2 col-form-label" >Fecha inicial</label>
-                        <input type="date" className="col-sm-3" id="fechaInicio"/>
+                        <input type="date" className="col-sm-3" name="fechaInicio"/>
                         <label name="IdSubasta" className="col-sm-2 col-form-label">Fecha Final</label>
-                        <input type="date" className="col-sm-4" id="fechaFin"/>
+                        <input type="date" className="col-sm-4" name="fechaFin"/>
                     </div>
                     <div className="row mb-3" bordercolor="blue">
                         <label name="nombre" className="col-sm-4 col-form-label" >Nombre del vehículo:</label>
-                        <input type="text" className="col-sm-7" id="nombre" placeholder="Nombre del vehículo" />
+                        <input type="text" className="col-sm-7" name="nombre" placeholder="Nombre del vehículo" />
                     </div>
                     <div className="mb-3">
                         <label for="formGroupExampleInput" className="form-label">Información / Descripción del vehículo: </label>
-                        <textarea type="text" className="form-control" id="formGroupExampleInput" placeholder="Agregue información del vehículo"></textarea>
+                        <textarea type="text" className="form-control" name="descripcion" placeholder="Agregue información del vehículo"></textarea>
                     </div>
                     <div className="mb-3">
                         <label for="formFileMultiple" className="form-label">Anexar fotos</label>
-                        <input className="form-control" type="file" id="formFileMultiple" multiple/>
+                        <input className="form-control" type="file" name="formFileMultiple" multiple/>
                     </div>
                 </div>
             </Form>
@@ -36,4 +36,3 @@ function DatosSubasta () {
 }
 
 export default DatosSubasta;
-
