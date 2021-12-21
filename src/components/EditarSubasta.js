0fe799dbba6de30b4d6690cbdce3c78ad03adc18
Marleidis <90942857+Marleidis@users.noreply.github.com>
2021-12-21 00:29:54 -0500

    Se actualiza vista

--- src/components/EditarSubasta.js
+++ src/components/EditarSubasta.js
@@ -3,10 +3,10 @@ import DatosSubasta from './DatosSubasta';
 function EditarSubasta () {
     return (
         <div>
-            <h3 className="text-center" >EDICIÓN DE SUBASTAS</h3>
+            <h3 className="text-center property mt-4 mb-3" >EDICIÓN DE SUBASTAS</h3>
                 <DatosSubasta />
                 <div className="text-center">
-                    <button type="submit" className="btn btn-primary">Editar Subasta</button>
+                    <button type="submit" className="btn btn-primary property mx-2">Editar Subasta</button>
                     <button type="submit" className="btn btn-secondary">Atrás</button>
                 </div>
         </div>
