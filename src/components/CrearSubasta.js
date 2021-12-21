ae3e4374ddcfceebc5797a0aa3003d1e807d729a
Marleidis <90942857+Marleidis@users.noreply.github.com>
2021-12-21 00:28:10 -0500

    Se actualiza vista

--- src/components/CrearSubasta.js
+++ src/components/CrearSubasta.js
@@ -3,10 +3,10 @@ import DatosSubasta from './DatosSubasta';
 function CrearSubasta () {
     return (
         <div>
-            <h3 className="text-center" >CREACIÓN DE SUBASTAS</h3>
+            <h3 className="property mt-4 mb-3" >CREACIÓN DE SUBASTAS</h3>
                 <DatosSubasta />
                 <div className="text-center">
-                    <button type="submit" className="btn btn-primary">Crear Subasta</button>
+                    <button type="submit" className="btn btn-primary property mx-2">Crear Subasta</button>
                     <button type="submit" className="btn btn-secondary">Atrás</button>
                 </div>
 
